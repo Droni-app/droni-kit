bee9bf68d7c86aab1d86c13b2f80883d1cd3f073
Gustavo Barragan <dev@droni.co>
2025-04-15 14:17:32 -0500

    feat: dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({ tsconfigPath: './tsconfig.app.json' })
+  ],
+  build: {
+    lib: {
+      entry: './src/main.ts',
+      name: 'droni-kit',
+      fileName: 'droni-kit',
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        },
+        dir: 'dist' 
+      }
+    }
+  },
 })
