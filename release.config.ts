9b621fbd639f5c276ea87b3a5b7c2330f4867df0
Gustavo Barragan <dev@droni.co>
2025-04-15 15:07:33 -0500

    fix: simple release

--- release.config.ts
+++ release.config.ts
@@ -1,23 +1,4 @@
 // release.config.ts
 export default {
-  branches: ["main"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-    "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-    "@semantic-release/github",
-  ],
+  branches: ['main', { name: 'beta', prerelease: true }],
 };
