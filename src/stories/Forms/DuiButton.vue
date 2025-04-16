4b20ca7772dba2c05f30b2d61b5f340b528e9ee3
Gustavo Barragan <dev@droni.co>
2025-04-15 21:28:12 -0500

    fix: button tailwind

--- src/stories/Forms/DuiButton.vue
+++ src/stories/Forms/DuiButton.vue
@@ -1,15 +1,17 @@
 <template>
-  <button type="button">
+  <button
+    type="button"
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
     {{ props.label }}
   </button>
 </template>
 
 <script lang="ts" setup>
+import '../../style.css';
 const props = defineProps({
   label: {
     type: String,
     default: 'Kalvin',
   },
 })
-
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    dts({ tsconfigPath: './tsconfig.app.json' })
+    dts({ tsconfigPath: './tsconfig.app.json' }),
+    tailwindcss()
   ],
   build: {
     lib: {
