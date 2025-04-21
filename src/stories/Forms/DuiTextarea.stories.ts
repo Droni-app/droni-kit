31031dfaa006ae146159310f5f3e63ecc3a0f3b5
Gustavo Barragan <dev@droni.co>
2025-04-21 18:56:09 -0500

    feat: textarea

---
+++ src/stories/Forms/DuiTextarea.stories.ts
@@ -0,0 +1,81 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import DuiTextarea from './DuiTextarea.vue';
+import '../../style.css';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: 'Forms/Textarea',
+  component: DuiTextarea,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  argTypes: {
+    block: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['sm', 'md', 'lg'],
+      defaultValue: 'md',
+    },
+    disabled: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+    placeholder: {
+      control: { type: 'text' },
+      defaultValue: undefined,
+    },
+    name: {
+      control: { type: 'text' },
+      defaultValue: undefined,
+    },
+    required: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+    label: {
+      control: { type: 'text' },
+      defaultValue: undefined,
+    },
+    rounded: {
+      constrol: { type: 'select' },
+      options: ['all', 'top', 'bottom', 'left', 'right', 'none'],
+      defaultValue: 'all',
+    },
+    autoheight: {
+      control: { type: 'boolean' },
+      defaultValue: true,
+    },
+    resize: {
+      control: { type: 'select' },
+      options: ['none', 'both', 'horizontal', 'vertical'],
+      defaultValue: 'none',
+    },
+  },
+  } satisfies Meta<typeof DuiTextarea>;
+  
+  export default meta;
+  type Story = StoryObj<typeof meta>;
+  /*
+   *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+   * See https://storybook.js.org/docs/api/csf
+   * to learn how to use render functions.
+   */
+  export const Default: Story = {
+    args: {
+      required: true,
+      disabled: false,
+      block: false,
+      size: 'md',
+      label: 'Ingresa tu correo',
+      id: 'correo',
+      placeholder: 'ej. kalvin@droni.co',
+      name: 'correo',
+      rounded: 'all',
+      modelValue: '',
+      autoheight: true,
+      resize: 'none',
+    },
+  };
---
+++ storybook-static/stories/Forms/DuiTextarea.stories.d.ts
@@ -0,0 +1,189 @@
+import { StoryObj } from '@storybook/vue3';
+declare const meta: {
+    title: string;
+    component: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
+        modelValue: {
+            type: StringConstructor;
+            default: string;
+        };
+        size: {
+            type: () => "sm" | "md" | "lg";
+            default: string;
+        };
+        block: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        disabled: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        placeholder: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        name: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        id: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        required: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        label: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        rounded: {
+            type: () => "all" | "top" | "bottom" | "left" | "right" | "none";
+            default: string;
+        };
+        autoheight: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        resize: {
+            type: () => "none" | "both" | "horizontal" | "vertical";
+            default: string;
+        };
+    }>, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {} & {
+        "update:modelValue": (value: string) => any;
+    }, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{
+        modelValue: {
+            type: StringConstructor;
+            default: string;
+        };
+        size: {
+            type: () => "sm" | "md" | "lg";
+            default: string;
+        };
+        block: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        disabled: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        placeholder: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        name: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        id: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        required: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        label: {
+            type: StringConstructor;
+            default: undefined;
+        };
+        rounded: {
+            type: () => "all" | "top" | "bottom" | "left" | "right" | "none";
+            default: string;
+        };
+        autoheight: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        resize: {
+            type: () => "none" | "both" | "horizontal" | "vertical";
+            default: string;
+        };
+    }>> & Readonly<{
+        "onUpdate:modelValue"?: ((value: string) => any) | undefined;
+    }>, {
+        size: "sm" | "md" | "lg";
+        disabled: boolean;
+        block: boolean;
+        label: string;
+        modelValue: string;
+        placeholder: string;
+        name: string;
+        id: string;
+        required: boolean;
+        rounded: "all" | "top" | "bottom" | "left" | "right" | "none";
+        autoheight: boolean;
+        resize: "none" | "both" | "horizontal" | "vertical";
+    }, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, HTMLDivElement>;
+    tags: string[];
+    argTypes: {
+        block: {
+            control: {
+                type: "boolean";
+            };
+            defaultValue: boolean;
+        };
+        size: {
+            control: {
+                type: "select";
+            };
+            options: string[];
+            defaultValue: string;
+        };
+        disabled: {
+            control: {
+                type: "boolean";
+            };
+            defaultValue: boolean;
+        };
+        placeholder: {
+            control: {
+                type: "text";
+            };
+            defaultValue: undefined;
+        };
+        name: {
+            control: {
+                type: "text";
+            };
+            defaultValue: undefined;
+        };
+        required: {
+            control: {
+                type: "boolean";
+            };
+            defaultValue: boolean;
+        };
+        label: {
+            control: {
+                type: "text";
+            };
+            defaultValue: undefined;
+        };
+        rounded: {
+            constrol: {
+                type: string;
+            };
+            options: string[];
+            defaultValue: string;
+        };
+        autoheight: {
+            control: {
+                type: "boolean";
+            };
+            defaultValue: boolean;
+        };
+        resize: {
+            control: {
+                type: "select";
+            };
+            options: string[];
+            defaultValue: string;
+        };
+    };
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+export declare const Default: Story;
