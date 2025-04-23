70e9fb77a3d41e760ef4f01bcdfcbb53a35e3219
Gustavo Barragan <dev@droni.co>
2025-04-22 23:37:58 -0500

    fix: user Worker

--- src/stories/Widgets/DuiEditor.vue
+++ src/stories/Widgets/DuiEditor.vue
@@ -4,25 +4,8 @@
 
 <script setup lang="ts">
 import { ref, onMounted, onBeforeUnmount, watch } from 'vue'
-import * as monaco from 'monaco-editor'
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
-
-// @ts-ignore
-self.MonacoEnvironment = {
-  getWorker(_: any, label: string) {
-    if (label === 'json') return new jsonWorker()
-    if (['css', 'scss', 'less'].includes(label)) return new cssWorker()
-    if (['html', 'handlebars', 'razor'].includes(label)) return new htmlWorker()
-    if (['typescript', 'javascript'].includes(label)) return new tsWorker()
-    return new editorWorker()
-  }
-}
-
-monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true)
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import '../../lib/userWorker'
 
 const props = defineProps({
   modelValue: {
