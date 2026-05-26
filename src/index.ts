0296e91a40ab4f805ba0439f47d3295b2d8654b2
Gustavo Barragan <dev@droni.co>
2026-05-25 22:54:54 -0500

    feat: new components export

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import DuiTextarea from './stories/Forms/DuiTextarea.vue'
 import DuiSelect from './stories/Forms/DuiSelect.vue'
 import DuiLabel from './stories/Forms/DuiLabel.vue'
 import DuiCheckbox from './stories/Forms/DuiCheckbox.vue'
+import DuiRadio from './stories/Forms/DuiRadio.vue'
+import DuiSwitch from './stories/Forms/DuiSwitch.vue'
 // Elements
 import DuiAlert from './stories/Elements/DuiAlert.vue'
 import DuiTable from './stories/Elements/DuiTable.vue'
@@ -12,6 +14,15 @@ import DuiAction from './stories/Elements/DuiAction.vue'
 import DuiCard from './stories/Elements/DuiCard.vue'
 import DuiModal from './stories/Elements/DuiModal.vue'
 import DuiTooltip from './stories/Elements/DuiTooltip.vue'
+import DuiAccordion from './stories/Elements/DuiAccordion.vue'
+import DuiBadge from './stories/Elements/DuiBadge.vue'
+import DuiDropdown from './stories/Elements/DuiDropdown.vue'
+import DuiDropdownItem from './stories/Elements/DuiDropdownItem.vue'
+import DuiDropdownSeparator from './stories/Elements/DuiDropdownSeparator.vue'
+import DuiPagination from './stories/Elements/DuiPagination.vue'
+import DuiSkeleton from './stories/Elements/DuiSkeleton.vue'
+import DuiTabs from './stories/Elements/DuiTabs.vue'
+import DuiToast from './stories/Elements/DuiToast.vue'
 // Widgets
 import DuiNavbar from './stories/Widgets/DuiNavbar.vue'
 
@@ -26,9 +37,20 @@ export {
   DuiSelect,
   DuiLabel,
   DuiCheckbox,
+  DuiRadio,
+  DuiSwitch,
   DuiAlert,
   DuiTable,
   DuiModal,
   DuiTooltip,
+  DuiAccordion,
+  DuiBadge,
+  DuiDropdown,
+  DuiDropdownItem,
+  DuiDropdownSeparator,
+  DuiPagination,
+  DuiSkeleton,
+  DuiTabs,
+  DuiToast,
   DuiNavbar
 }
